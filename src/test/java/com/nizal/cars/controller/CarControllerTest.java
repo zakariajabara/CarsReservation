dee143b6822fabf7a478a3037ec40503063054d6
j.zakaria@hotmail.de <j.zakaria@hotmail.de>
2022-03-01 20:30:19 +0100

    second commit

--- src/test/java/com/nizal/cars/controller/CarControllerTest.java
+++ src/test/java/com/nizal/cars/controller/CarControllerTest.java
@@ -18,27 +18,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-/*
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nizal.cars.dto.CarDTO;
-import com.nizal.cars.repository.CarJpaRepository;
-*/
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nizal.cars.dto.CarDTO;
 import com.nizal.cars.repository.CarJpaRepository;
